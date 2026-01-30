a58a9d5aa6cb031b01e2b27e04182702b9fed531
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-01-30 12:22:25 +0530

    third commit

--- invertedandrotatedhalfpyramid.java
+++ invertedandrotatedhalfpyramid.java
@@ -1,14 +1,14 @@
 public class invertedandrotatedhalfpyramid {
     public static void InvandRotatePyramid(int n){
         for(int i = 1; i<=n;i++){
-            for(int j = 1; j<=n;j++){
-                if(j<=n-i){
-                    System.out.print(" ");
-                }
-                else{
-                    System.out.print("*");
-                }
-                
+            // spaces
+            for(int j = 1; j<=n-i;j++){
+                System.out.print(" ");
+            }
+
+            // stars
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
             }
             System.out.println();
         }
